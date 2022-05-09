266402cca5f1ca6bd9351b1e836cf66ea8329edf
Nicolas Echeverry <nikoxx9900@gmail.com>
2022-05-09 18:16:01 -0500

    Added debugger info on dev mode

---
+++ front-laniakea/components/main/Debugger.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <v-row class="justify-center">
+      <pre>
+      {{ JSON.stringify($store.state.sessionHandler, null, 4) }}
+    </pre>
+    </v-row>
+    <v-row class="justify-center">
+      <code>
+        {{ $config }}
+      </code>
+    </v-row>
+  </v-container>
+</template>
